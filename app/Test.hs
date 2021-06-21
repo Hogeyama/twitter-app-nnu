5d33320098afd70f34ce2fafb662ad0fd721e498
Hogeyama <gan13027830@gmail.com>
2021-06-21 23:40:37 +0900

    Remove unused file

--- app/Test.hs
+++
@@ -1,9 +0,0 @@
-module Main (main) where
-
-import           RIO
-import           NNU.Logger as Logger
-import           Data.Generics.Labels ()
-
-main :: IO ()
-main = runRIO defaultLogFunc' $ do
-  logW $ Just (1 :: Int)
