f70723cc7c3fa85d85e9affbddd681d0c9b636f8
Hogeyama <gan13027830@gmail.com>
2021-07-29 01:47:05 +0900

    Add log

--- app/Main.hs
+++ app/Main.hs
@@ -4,12 +4,14 @@ module Main
 
 import           RIO
 
+import qualified Data.Aeson                    as J
 import           GHC.IO.Encoding                ( setFileSystemEncoding
                                                 , setLocaleEncoding
                                                 , utf8
                                                 )
-import qualified Data.Aeson                    as J
-import           System.ReadEnvVar              ( readEnvDef )
+import           System.ReadEnvVar              ( lookupEnv
+                                                , readEnvDef
+                                                )
 
 import qualified NNU.App.TwitterBot            as Bot
 import qualified NNU.Logger                    as Logger
@@ -20,10 +22,15 @@ main = do
   setLocaleEncoding utf8
   setFileSystemEncoding utf8
   runRIO Logger.defaultLogFunc' $ logAnyError $ do
-    isTest <- readEnvDef "NNU_TEST" False
+    isTest       <- readEnvDef "NNU_TEST" False
+    cfgTableName <- lookupEnv "NNU_TABLE_NAME" >>= \case
+      Just t  -> pure t
+      -- テーブルを間違えると悲惨なことになるので死ぬべき
+      Nothing -> throwString "NNU_TABLE_NAME not set"
     Logger.info $ J.object
       [ "msg" J..= ("NNU on AWS has started" :: Text)
       , "is_test" J..= isTest
+      , "NNU_TABLE_NAME" J..= (cfgTableName :: Text)
       ]
     let appConfigs = if isTest
           then [Bot.testAppConfig]
