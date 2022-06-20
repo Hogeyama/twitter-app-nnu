edbf40b71eb0407b317324d4a86dd46f4f63b16e
Hogeyama <gan13027830@gmail.com>
2022-06-20 22:17:56 +0900

    feat(lib): Remove Slack notification from Logger
    Has been replaced by CloudWatch Logs + Lambda + SNS

--- lib/NNU/Effect/Log/StdoutLogImpl.hs
+++ lib/NNU/Effect/Log/StdoutLogImpl.hs
@@ -6,7 +6,6 @@ import Polysemy
 
 import Data.Aeson ((.=))
 import qualified Data.Aeson as A
-import Network.HTTP.Simple as HTTP
 import qualified RIO.Text as T
 
 import NNU.Effect.Log
@@ -34,19 +33,7 @@ runLog = interpret $ \case
             , "revision" A..= ($(TH.revision) :: Text)
             ]
         msgT = decodeUtf8Lenient $ toStrictBytes $ A.encode msg
-    when (logLevel `elem` [LevelInfo, LevelError]) $ notifySlack msgT
     hPutBuilder stderr $ encodeUtf8Builder msgT <> "\n"
   where
     -- Slack で :5: とかが絵文字になってしまう
     replaceColon = T.map (\c -> if c == ':' then '-' else c)
-
-    -- TODO Slack への通知は SNS 経由で行うようにする
-    notifySlack msg = do
-      let url =
-            "https://hooks.slack.com/services/***REMOVED***"
-          body = A.object ["text" A..= msg]
-      req <- liftIO $ do
-        HTTP.parseRequest url
-          <&> HTTP.setRequestMethod "POST"
-          <&> setRequestBodyJSON body
-      void $ httpNoBody req
