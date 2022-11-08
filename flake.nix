e71e49dcf300f04887e9cb16dede7cbe1d976bcc
Hogeyama <gan13027830@gmail.com>
2022-11-08 17:13:24 +0900

    chore: Add newer nixpkgs as nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/master";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils/master";
     flake-compat = {
       url = github:edolstra/flake-compat;
@@ -10,18 +11,20 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils, ... }:
     let
       compiler = "ghc8107";
       supportedSystems = [ "x86_64-linux" ];
 
-      outputs-overlay = pkgs: prev: {
-        my-package = import ./nix/my-package.nix { inherit pkgs compiler; };
-        my-shell = import ./nix/my-shell.nix { inherit pkgs compiler; };
-      };
     in
     flake-utils.lib.eachSystem supportedSystems (system:
       let
+        outputs-overlay = pkgs: prev: {
+          # unstable packages are available as pkgs.unstable.${package}
+          unstable = nixpkgs-unstable.outputs.legacyPackages.${system};
+          my-package = import ./nix/my-package.nix { inherit pkgs compiler; };
+          my-shell = import ./nix/my-shell.nix { inherit pkgs compiler; };
+        };
         pkgs = import nixpkgs {
           inherit system;
           overlays = [ outputs-overlay ];
